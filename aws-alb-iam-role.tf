498bc871b3342d2c6f234a34f434381bf0f17b6c
jhong40 <jhong40@gmail.com>
2023-06-27 13:21:06 -0400

    Update and rename oidc.tf to aws-alb-iam-role.tf

---
+++ aws-alb-iam-role.tf
@@ -0,0 +1,87 @@
+# Use this data source to lookup information about the current AWS partition in which Terraform is working
+data "aws_partition" "current" {}
+
+# Resource: AWS IAM Open ID Connect Provider
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
+  thumbprint_list = []
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  tags = merge(
+    {
+      Name = "${var.cluster_name}-eks-irsa"
+    },
+    local.common_tags
+  )
+}
+
+# Output: AWS IAM Open ID Connect Provider ARN
+output "aws_iam_openid_connect_provider_arn" {
+  description = "AWS IAM Open ID Connect Provider ARN"
+  value       = aws_iam_openid_connect_provider.oidc_provider.arn
+}
+
+# Extract OIDC Provider from OIDC Provider ARN
+locals {
+  aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
+}
+
+# Output: AWS IAM Open ID Connect Provider
+output "aws_iam_openid_connect_provider_extract_from_arn" {
+  description = "AWS IAM Open ID Connect Provider extract from ARN"
+  value       = local.aws_iam_oidc_connect_provider_extract_from_arn
+}
+
+
+#############
+# Resource: Create AWS Load Balancer Controller IAM Policy 
+resource "aws_iam_policy" "lbc_iam_policy" {
+  name        = "${local.name}-AWSLoadBalancerControllerIAMPolicy"
+  path        = "/"
+  description = "AWS Load Balancer Controller IAM Policy"
+  policy      = data.http.lbc_iam_policy.body
+}
+
+output "lbc_iam_policy_arn" {
+  value = aws_iam_policy.lbc_iam_policy.arn
+}
+
+# Resource: Create IAM Role 
+resource "aws_iam_role" "lbc_iam_role" {
+  name = "${local.name}-lbc-iam-role"
+
+  # Terraform's "jsonencode" function converts a Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = "${data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn}"
+        }
+        Condition = {
+          StringEquals = {
+            "${data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_extract_from_arn}:aud" : "sts.amazonaws.com",
+            "${data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_extract_from_arn}:sub" : "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "AWSLoadBalancerControllerIAMPolicy"
+  }
+}
+
+# Associate Load Balanacer Controller IAM Policy to  IAM Role
+resource "aws_iam_role_policy_attachment" "lbc_iam_role_policy_attach" {
+  policy_arn = aws_iam_policy.lbc_iam_policy.arn
+  role       = aws_iam_role.lbc_iam_role.name
+}
+
+output "lbc_iam_role_arn" {
+  description = "AWS Load Balancer Controller IAM Role ARN"
+  value       = aws_iam_role.lbc_iam_role.arn
+}
